09d1ee4216328b481a9b577f36be3c546f984bd2
Alexey Utkin <cedrickobayashi@gmail.com>
2025-12-06 01:50:31 +0300

    Task 1 is done

---
+++ src/test/java/ru/netology/web/CardDeliveryTest.java
@@ -0,0 +1,44 @@
+package ru.netology.web;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Selenide;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
+
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selectors.*;
+import static com.codeborne.selenide.Selenide.*;
+
+class CardDeliveryTest {
+
+    public String generateDate(int days, String pattern) {
+        return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern(pattern));
+    }
+
+    @Test
+    void shouldSendAForm() {
+        String planningDate = generateDate(4, "dd.MM.yyyy");
+
+        Selenide.open("http://localhost:9999");
+        $("[data-test-id='city'] input")
+                .press(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE)
+                .setValue("Пенза");
+        $("[data-test-id='date'] input")
+                .press(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE)
+                .setValue(planningDate);
+        $("[data-test-id='name'] input")
+                .press(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE)
+                .setValue("Петров Иван");
+        $("[data-test-id='phone'] input")
+                .press(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE)
+                .setValue("+79876543210");
+        $("[data-test-id='agreement']").click();
+        $$("button").filter(Condition.visible).find(text("Забронировать")).click();
+        $("[data-test-id='notification']").should(Condition.text("Встреча успешно забронирована на " + planningDate),
+                        Duration.ofSeconds(15)).should(Condition.visible);
+    }
+}
